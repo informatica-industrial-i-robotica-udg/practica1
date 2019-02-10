49d5fe8ffc7c6b4a4e82facb4c002de82492e79b
Marc Sànchez <msanxes@gmail.com>
2019-02-10 12:30:17 +0100

    [FEAT] Veure com demanar nombres per terminal i com aplicar operacions a vectors

---
+++ pintarArray.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+n = int (input ("Entreu el valor de n: "))
+# Exemple treballant amb vectors.
+a = np.array([1, 2, 3])
+b = np.array([4,5,6])
+# Aplica l'operació a tot el vector.
+print(a+b,a*n,-b)
